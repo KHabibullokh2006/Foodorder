ee88aee97336c79d87783ac2a214748a21ae4534
Khabibullokh <68428742+KHabibullokh2006@users.noreply.github.com>
2023-03-12 01:17:45 +0500

    Update OverviewActivity.kt

--- app/src/main/java/uz/itschool/foodorder/OverviewActivity.kt
+++ app/src/main/java/uz/itschool/foodorder/OverviewActivity.kt
@@ -9,6 +9,20 @@ class OverviewActivity : AppCompatActivity() {
         val binding = ActivityOverviewBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        
+        var img = intent.getStringExtra("img")
+        var name = intent.getStringExtra("name")
+        var desc = intent.getStringExtra("desc")
+        var price = intent.getStringExtra("price")
+        
+        binding.img.load(img)
+        binding.product_name.text = name
+        binding.product_desc.text = desc
+        binding.product_price.text = price
+        
+        binding.button.setOnClickListener{
+            Toast.makeText(this, "Meal was added to cart", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
